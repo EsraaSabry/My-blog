5d674d1517579208525e207349bd816f153d2447
esraa <es300093@gmail.com>
2022-07-19 00:45:47 +0200

    add modifications to temp

--- myblog/models.py
+++ myblog/models.py
@@ -10,6 +10,7 @@ from django.conf import settings
 class Post(models.Model):
     auther = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
+    text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
